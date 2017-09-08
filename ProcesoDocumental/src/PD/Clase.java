3067e332d39e64e86721e7011bae8cad5bf533bc
miguelm197 <miguelmax197@gmail.com>
2017-09-08 15:21:10 -0300

    Inicio Proceso Documental

---
+++ ProcesoDocumental/src/PD/Clase.java
@@ -0,0 +1,14 @@
+package PD;
+
+import com.dogma.busClass.ApiaAbstractClass;
+import com.dogma.busClass.BusClassException;
+import com.dogma.busClass.object.Attribute;
+import com.dogma.busClass.object.PossibleValue;
+
+public class Clase extends ApiaAbstractClass{
+	@Override
+	protected void executeClass() throws BusClassException {
+		
+		
+	}
+}
